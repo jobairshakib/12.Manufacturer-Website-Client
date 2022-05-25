3cb3b806527b8f4453d9e263b7f7011cd7ff2339
jobairshakib <you@example.comshakib.jobair@gmail.com>
2022-05-25 16:37:13 +0600

    err0r404 pagee added

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import error404 from '../images/err404.png'
 
 const Error404 = () => {
     return (
-        <div>
-            <h2>This is Error</h2>
-        </div>
+        <>
+            <section className="bg-base-100">
+                <div className="flex justify-center items-center">
+                    <img src={error404} alt="" />
+                </div>
+                <h2 className="tex-3xl text-center font-bold">404 ERROR. Page Not Found</h2>
+                <div className='w-screen text-center mt-4'>
+                    <Link to="/">
+                        <button className="btn uppercase btn-primary">
+                            Back to The home
+                        </button>
+                    </Link>
+                </div>
+            </section>
+        </>
     );
 };
 
