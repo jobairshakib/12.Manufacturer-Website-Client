5e9420e67b0fbe3ff72472464e352fce1368f25a
jobairshakib <you@example.comshakib.jobair@gmail.com>
2022-05-26 21:38:41 +0600

    admin added

--- src/Components/Dashboard/MyOrder.js
+++ src/Components/Dashboard/MyOrder.js
@@ -61,12 +61,12 @@ const MyOrder = () => {
                     {/* <!-- head --> */}
                     <thead>
                         <tr>
-                            <th className='text-primary-focus'>Parts Name</th>
-                            <th className='text-primary-focus'>Parts Quantity</th>
-                            <th className='text-primary-focus'>Unit Price</th>
-                            <th className='text-primary-focus'>Total Price</th>
-                            <th className='text-primary-focus'>Payment</th>
-                            <th className='text-primary-focus'>Delete</th>
+                            <th className='text-primary-focus text-base'>Parts Name</th>
+                            <th className='text-primary-focus text-base'>Parts Quantity</th>
+                            <th className='text-primary-focus text-base'>Unit Price</th>
+                            <th className='text-primary-focus text-base'>Total Price</th>
+                            <th className='text-primary-focus text-base'>Payment</th>
+                            <th className='text-primary-focus text-base'>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -77,8 +77,8 @@ const MyOrder = () => {
                                     <td>{o.purchaseQuantity}</td>
                                     <td>{o.pricePerUnit} ৳</td>
                                     <td>{(o.pricePerUnit) * (o.purchaseQuantity)} ৳</td>
-                                    <td><button className='btn btn-success gap-2'>Payment<FontAwesomeIcon icon={faMoneyBill1Wave}/></button></td>
-                                    <td><button onClick={() => handleDelete(o._id)} className='btn gap-2 btn-error'>Delete <FontAwesomeIcon icon={faTrash} /></button></td>
+                                    <td><button className='btn btn-xs btn-success gap-2'>Payment<FontAwesomeIcon icon={faMoneyBill1Wave}/></button></td>
+                                    <td><button onClick={() => handleDelete(o._id)} className='btn gap-2 btn-xs btn-error'>Delete <FontAwesomeIcon icon={faTrash} /></button></td>
                                 </tr>
                                 )
                         }
--- src/Components/Dashboard/Users.js
+++ src/Components/Dashboard/Users.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Loading/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
+    const { data: users, isLoading,refetch } = useQuery('users', () => fetch(`http://localhost:5000/user`, {
+        headers: {
+            authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json())); 
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+
     return (
         <div>
-            <h2>Users</h2>
+            <h2>Users : {users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th className='text-primary-focus text-base'></th>
+                            <th className='text-primary-focus text-base'>Name</th>
+                            <th className='text-primary-focus text-base'>ROle</th>
+                            <th className='text-primary-focus text-base'>Remove User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <UserRow
+                                key={user._id}
+                                user={user}
+                                refetch={refetch}
+                            ></UserRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
